83552fa3285592906fc433dceb4337c82617eff3
Thilo <thilo0123@gmail.com>
2026-02-19 00:23:10 +0100

    Pass through npm package version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,58 +2,59 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  ssr: false ,
+  ssr: false,
   components: [
     {
-      path: '~/components',
+      path: "~/components",
       pathPrefix: false,
-    }
+    },
   ],
   icon: {
-    mode: 'css',
-    cssLayer: 'base'
+    mode: "css",
+    cssLayer: "base",
   },
   nitro: {
     devProxy: {
-      '/api': {
-        target: 'http://localhost:8080/api',
+      "/api": {
+        target: "http://localhost:8080/api",
         changeOrigin: true,
         secure: false,
       },
     },
   },
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/hints',
-    '@nuxt/image',
-    '@nuxt/icon',
-    '@nuxtjs/google-fonts',
-    '@nuxtjs/i18n',
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/hints",
+    "@nuxt/image",
+    "@nuxt/icon",
+    "@nuxtjs/google-fonts",
+    "@nuxtjs/i18n",
   ],
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE, 
-    },  
+      apiBase: process.env.NUXT_PUBLIC_API_BASE,
+      appVersion: process.env.npm_package_version,
+    },
   },
   i18n: {
     compilation: {
       strictMessage: false,
     },
     locales: [
-      { code: 'en', language: 'en-US', file: 'en.json' },
-      { code: 'de', language: 'de-DE', file: 'de.json' },
-      { code: 'fr', language: 'fr-FR', file: 'fr.json' },
-      { code: 'it', language: 'it-IT', file: 'it.json' }
+      { code: "en", language: "en-US", file: "en.json" },
+      { code: "de", language: "de-DE", file: "de.json" },
+      { code: "fr", language: "fr-FR", file: "fr.json" },
+      { code: "it", language: "it-IT", file: "it.json" },
     ],
-    strategy: 'no_prefix',
-    defaultLocale: 'en',
-    vueI18n: './i18n.config.ts',
+    strategy: "no_prefix",
+    defaultLocale: "en",
+    vueI18n: "./i18n.config.ts",
     detectBrowserLanguage: {
       useCookie: true,
-      cookieKey: 'lang',
+      cookieKey: "lang",
     },
   },
   googleFonts: {
@@ -65,7 +66,6 @@ export default defineNuxtConfig({
   },
   vite: {
     plugins: [tailwindcss()],
-    
   },
-  css: ['./app/assets/css/main.css'],
-})
+  css: ["./app/assets/css/main.css"],
+});
